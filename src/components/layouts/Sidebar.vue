96807abd49ab60e702480d214354b06c7d256fea
EremenkoVO <eremenko.v.o@yandex.ru>
2021-11-05 15:45:33 +0300

    init project

---
+++ src/components/layouts/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sidebar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+};
+</script>
+<style lang="scss" scoped>
+.sidebar {
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 15px;
+  width: 250px;
+  background-color: black;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+</style>
